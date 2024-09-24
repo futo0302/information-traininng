155a7f50696bf34a4a4ef9701c7ac92ebe2b461e
futo0302 <futo3232@icloud.com>
2024-09-24 03:56:19 +0000

    ９月２４日todoリスト

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const todoList = document.getElementById('todoList');
-function addTodo() {
+document.addEventListener('DOMContentLoaded', function(){
+    const todoList = document.getElementById('todoList');
+    loadTodos();
+    function saveTodoLocalStorage(todoText){
+        let todos =JSON.parse(localStorage.getItem('todos'))  || [];
+        todos.push(todoText);
+        localStorage.setItem('todos', JSON.stringify('todos'))
+}
+function loadTodos(){
+    let todos = JSON.parse(localStorage.getItem('todos')) || [] 
+    todos.forEach(function(todo){
+        addTodoToList(todo);
+    });
+}
+window.addTodo = function(){
     const todoInput = document.getElementById('todoInput');
     const todoText = todoInput.value.trim();
 
-    if (todoText !== "") {
+    if (todoText !== " "){
         addTodoToList(todoText);
-        todoInput.value = '';
-    } else {
+        saveTodoLocalStorage(todoText);
+            todoInput.value = '';
+    }else {
         alert("ToDoを入力してください。");
     }
 };
@@ -14,4 +28,5 @@ function addTodoToList(todoText) {
     const li = document.createElement('li');
     li.textContent = todoText;
     todoList.appendChild(li);
-}
+    }
+});
